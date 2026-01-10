6948a057ab0391f3078bb6399dec6ffb1efd1411
Abdisa.dev <meetabdisa@gmail.com>
2026-01-10 04:42:41 +0300

    Navbar and add all UI needed for the whole project

---
+++ components/general/Navbar.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import Link from "next/link";
+import Logo from "@/public/logo.png";
+import { ThemeToggle } from "./ThemeToggle";
+import { buttonVariants } from "../ui/button";
+
+export default function Navbar() {
+  return (
+    <nav className="flex justify-between items-center py-5">
+      <Link href="/" className="flex items-center gap-2">
+        <Image src={Logo} alt="Pixel careers Logo" width={40} height={40} />
+        <h1 className="text-2xl font-bold">
+          Pixel<span className="text-primary">Careers</span>
+        </h1>
+      </Link>
+      <div className="flex items-center gap-4">
+        <ThemeToggle />
+        <Link
+          href="/login"
+          className={buttonVariants({ variant: "outline", size: "lg" })}
+        >
+          Login
+        </Link>
+      </div>
+    </nav>
+  );
+}
